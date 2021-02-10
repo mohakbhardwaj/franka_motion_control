3de4cf5bc937fcba1d860bf2c1bd514fd47e96a1
Mohak Bhardwaj <mohakb@cs.washington.edu>
2021-02-09 21:22:41 -0800

    add seperate control loops for spline and not spline

--- src/mpc.py
+++ src/mpc.py
@@ -5,7 +5,7 @@ import rospy
 import rospkg
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState
-import time
+from std_msgs.msg import String
 import torch
 torch.multiprocessing.set_start_method('spawn',force=True)
 
@@ -38,13 +38,13 @@ x_des_list = [franka_bl_state, franka_br_state]#, drop_state, home_state]
 
 class MPCController(object):
     def __init__(self, robot_state_topic, command_topic, goal_topic, mpc_yml_file, goal_state_list, control_freq, debug=False):
+        #user_command_topic
         self.robot_state_topic = robot_state_topic
         self.command_topic = command_topic
         self.goal_topic = goal_topic
+        # self.user_command_topic = user_command_topic
         self.mpc_yml_file = mpc_yml_file
         self.control_freq = control_freq
-        # with open(mpc_yml_file) as file:
-        #     self.self.exp_params = yaml.load(file, Loader=yaml.FullLoader)
 
         self.goal_state_list = goal_state_list
         self.curr_goal_idx = 0
@@ -52,13 +52,11 @@ class MPCController(object):
 
         #Initialize MPC controller
         self.initialize_mpc_controller()
-
         self.control_process.update_goal(goal_state=self.curr_goal)
         self.controller.rollout_fn.update_goal(goal_state=self.curr_goal)
 
-        self.robot_command_filter = JointStateFilter(filter_coeff={'position': 1.0, 'velocity': 0.2})
-        #dt=self.exp_params['control_dt']
-                                                     
+        #Create filters for state and command
+        self.robot_command_filter = JointStateFilter(filter_coeff={'position': 1.0, 'velocity': 0.2})                                                     
         self.robot_state_filter = JointStateFilter(filter_coeff={'position': 1.0, 'velocity': 0.1}, dt=0.0)
         self.spline = CubicSplineInterPolation()
         rospy.loginfo('[MPC]: Controller Initialized')
@@ -67,6 +65,7 @@ class MPCController(object):
         self.pub = rospy.Publisher(self.command_topic, JointState, queue_size=1, latch=False)
         self.state_sub = rospy.Subscriber(self.robot_state_topic, JointState, self.state_callback)
         self.goal_sub = rospy.Subscriber(self.goal_topic, PoseStamped, self.goal_callback)
+        # self.user_command_sub = rospy.Subscriber(self.user_command_topic, String, self.user_command_callback)
         self.rate = rospy.Rate(self.control_freq)
 
         #Variables for book-keeping
@@ -76,7 +75,7 @@ class MPCController(object):
         self.curr_state_filtered_dict = {}
         self.curr_ee_goal = None
         self.curr_mpc_command = JointState()
-        self.start_control = False
+        self.stop_controller = False
         self.zero_acc = np.zeros(7)
         self.prev_acc = np.zeros(7)
         self.tstep = 0
@@ -92,6 +91,11 @@ class MPCController(object):
             self.command_qdd_list = []
             self.ee_dist_err_list = []
             self.ee_rot_err_list = []
+
+    # def user_command_callback(self, msg):
+    #     if msg.data == "stop":
+    #         self.stop_controller = True
+    #         rospy.loginfo('Stop command received')
 
     def state_callback(self, msg):
         self.curr_state_raw = msg
@@ -121,7 +125,7 @@ class MPCController(object):
         # self.controller.rollout_fn.update_goal(goal_ee_pos = goal_ee_pos,
         #                                        goal_ee_quat = goal_ee_quat)
     
-    def control_loop(self):
+    def control_loop_spline(self):
         
         while not rospy.is_shutdown():
         
@@ -132,15 +136,17 @@ class MPCController(object):
                                            self.prev_acc))
                 # curr_state_np = np.concatenate((self.curr_state_filtered_dict['position'], 
                 #                                 self.curr_state_filtered_dict['velocity'], 
-                #                                 self.prev_acc), axis=1)
-                # print(curr_state_np.shape)
+                #                                 self.prev_acc), axis=0)
+
                 curr_state_tensor = torch.as_tensor(curr_state_np)
                 next_command, command_dt, val, info = self.control_process.get_command(self.tstep, curr_state_tensor,
-                                                                            debug=False,
-                                                                            control_dt=self.control_process.mpc_dt)
-                if not self.spline.command_available(self.tstep):
-
-                                                                            
+                                                                                        debug=False,
+                                                                                        control_dt=self.control_process.mpc_dt)
+                
+                # print(self.control_process.mpc_dt)
+                
+                #calculate new spline command if unavailable
+                if not self.spline.command_available(self.tstep):                                                                            
                     # if(self.exp_params['control_space'] == 'acc'):
                     qdd_des = np.ravel(next_command[0])
                     curr_command_dt = command_dt[0].item()
@@ -172,8 +178,7 @@ class MPCController(object):
 
 
                 spline_cmd_des = self.spline.get_command(self.tstep)
-                # print(spline_cmd_des)
-                self.curr_mpc_command.position = spline_cmd_des[0]
+                self.curr_mpc_command.position = spline_cmd_des[0].detach().numpy()
                 self.curr_mpc_command.velocity = np.zeros(7) #spline_cmd_des[1] #return spline derivative as well   
                 self.curr_mpc_command.effort = np.zeros(7) #What is the third key here??
 
@@ -181,9 +186,78 @@ class MPCController(object):
                 rospy.loginfo('[MPC]: Command published')
                 
                 if self.tstep == 0:
-                    self.start_t = time.time()
+                    self.start_t = rospy.get_time()
+                    # self.start_t = time.time()
 
-                self.tstep = time.time() - self.start_t #TODO: Check this versus ros::Time now and state timestamp
+                # self.tstep = time.time() - self.start_t 
+                self.tstep = rospy.get_time() - self.start_t
+
+                # print(self.tstep, time.time(), rospy.get_time(), rospy.get_time()-self.start_t_ros)
+                # ee_error,_ = self.controller.rollout_fn.current_cost(curr_state_tensor.unsqueeze(0))
+                # ee_error = [x.detach().cpu().item() for x in ee_error]
+                # rospy.loginfo(["{:.3f}".format(x) for x in ee_error]) #, "{:.3f}".format(mpc_control.mpc_dt)
+                
+                if self.debug:
+                    self.log_data(qdd_des, [0., 0., 0.])
+
+            elif self.curr_state_raw is None:
+                rospy.loginfo('[MPC]: Waiting for state')
+            
+            elif self.curr_ee_goal is None:
+                rospy.loginfo('[MPC]: Waiting for goal`')
+
+            self.rate.sleep()
+
+        self.close()
+    
+
+    def control_loop_linear(self):
+
+        while not rospy.is_shutdown():
+        
+            if self.curr_state_raw is not None: # and self.curr_ee_goal is not None:
+                
+                curr_state_np = np.hstack((self.curr_state_filtered_dict['position'], 
+                                           self.curr_state_filtered_dict['velocity'], 
+                                           self.prev_acc))
+                # curr_state_np = np.concatenate((self.curr_state_filtered_dict['position'], 
+                #                                 self.curr_state_filtered_dict['velocity'], 
+                #                                 self.prev_acc), axis=1)
+                curr_state_tensor = torch.as_tensor(curr_state_np)
+                next_command, command_dt, val, info = self.control_process.get_command(self.tstep, curr_state_tensor,
+                                                                            debug=False,
+                                                                            control_dt=self.control_process.mpc_dt)
+                                             
+                if(self.exp_params['control_space'] == 'acc'):
+                    qdd_des = np.ravel(next_command[0])
+                    curr_command_dt = command_dt[0].item()
+                    # print('Curr command dt', curr_command_dt, self.tstep)
+                    mpc_cmd_des = self.robot_command_filter.integrate_acc(qdd_des, 
+                                            self.curr_state_filtered_dict,
+                                            dt = 10*(curr_command_dt - self.tstep))
+                    self.curr_mpc_command.position = mpc_cmd_des['position']
+                    self.curr_mpc_command.velocity = mpc_cmd_des['velocity']
+                    self.curr_mpc_command.effort = np.zeros(7) #What is the third key here??
+                    self.prev_acc = qdd_des
+                    
+                elif(self.exp_params['control_space'] == 'pos'):
+                    self.curr_mpc_command.position = np.ravel(next_command[0])
+                    self.curr_mpc_command.velocity = np.zeros(7)
+                    self.curr_mpc_command.effort = np.zeros(7)
+                    
+                    # mpc_des_state_np = np.concatenate(([mpc_cmd_des['position']],
+                    #                                    [mpc_cmd_des['velocity']]),
+                    #                                    axis=0)
+    
+                self.pub.publish(self.curr_mpc_command)
+                rospy.loginfo('[MPC]: Command published')
+                
+                if self.tstep == 0:
+                    # self.start_t = time.time()
+                    self.start_t = rospy.get_time()
+
+                # self.tstep = time.time() - self.start_t
+                self.tstep = rospy.get_time() - self.start_t
                 
                 # ee_error,_ = self.controller.rollout_fn.current_cost(curr_state_tensor.unsqueeze(0))
                 # ee_error = [x.detach().cpu().item() for x in ee_error]
@@ -316,4 +390,4 @@ if __name__ == '__main__':
     #               Press Enter to continue... \n""")
     # input()
     rospy.loginfo('[MPC]: Initiating Control Loop')
-    mpc_controller.control_loop()
+    mpc_controller.control_loop_spline()
