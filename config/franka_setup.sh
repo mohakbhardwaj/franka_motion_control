0fe9d9f6a397cda36cff667b3416da021d27a715
Mohak Bhardwaj <mohakb@cs.washington.edu>
2021-03-29 14:28:01 -0700

    move_to_config works

--- config/franka_setup.sh
+++ config/franka_setup.sh
@@ -1,17 +1,15 @@
 export robot_ip="172.16.0.2"
 #### Home configs ####
-HOME_CONFIG_1=(0.0 -0.7853 0.0 -2.3561 0.0 1.5707 0.7853)
-HOME_CONFIG_2=(-0.207 -0.494 -0.398 -2.239 1.289 1.194 0.462)
-HOME_CONFIG_3=(1.745 0.914 -2.254 -2.306 0.784 1.617 -0.109) 
-HOME_CONFIG_4=(-0.9 -0.9 1.2 -1.9 -0.2 1.57 0.78) 
+HOME_CONFIG_1="[0.0, -0.7853, 0.0, -2.3561, 0.0, 1.5707, 0.7853]"
+HOME_CONFIG_2="[-0.207, -0.494, -0.398, -2.239, 1.289, 1.194, 0.462]"
+HOME_CONFIG_3="[1.745, 0.914, -2.254, -2.306, 0.784, 1.617, -0.109]"
+HOME_CONFIG_4="[-0.9, -0.9, 1.2, -1.9, -0.2, 1.57, 0.78]"
 
 
-export home_1=${HOME_CONFIG_1[@]}
-export home_2=${HOME_CONFIG_2[@]}
-export home_3=${HOME_CONFIG_3[@]}
-export home_4=${HOME_CONFIG_4[@]}
-
+export home_1=${HOME_CONFIG_1}
+export home_2=${HOME_CONFIG_2}
+export home_3=${HOME_CONFIG_3}
+export home_4=${HOME_CONFIG_4}
 
 
 #######Aliases for commands########
-# alias move_to= 
