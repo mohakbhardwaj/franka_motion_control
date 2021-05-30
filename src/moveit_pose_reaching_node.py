25452c922f35b5cd72378521c67693f96cc417c5
Mohak Bhardwaj <mohakb@cs.washington.edu>
2021-05-29 17:00:27 -0700

    moveit node working'

--- src/moveit_pose_reaching_node.py
+++ src/moveit_pose_reaching_node.py
@@ -10,17 +10,19 @@ from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 from moveit_commander.conversions import pose_to_list
+import random
+import numpy as np
+random.seed(0)
+np.random.seed(0)
+from datetime import datetime
+
+
 
 class MoveitReacher(object):
     def __init__(self):
         self.robot_state_topic = "joint_states"
         self.ee_goal_topic = "ee_goal"
-        #Setup Moveit
-        self.robot = moveit_commander.RobotCommander()
-        # self.scene = moveit_commander.PlanningSceneInterface()
-        self.move_group_arm = moveit_commander.MoveGroupCommander("panda_arm")
-        # self.move_group_hand = moveit_commander.MoveGroupCommander("hand")
-        self.log_data = False
+        self.log_data = True
         
         self.goal_ee_pos_list= [[ 3.8689e-01,  4.4958e-01,  5.9739e-01],
         [ 4.7659e-01,  4.9783e-01,  2.8616e-01],
@@ -35,19 +37,45 @@ class MoveitReacher(object):
         [ 0.1254,  0.6598,  0.7397,  0.0417],
         [ 0.1133, -0.0171,  0.9911,  0.0680],
         [ 0.0000,  0.7055,  0.7087,  0.0000]]
+
+        self.curr_goal_idx = 0
+        self.num_goals = len(self.goal_ee_pos_list)
+    
+        #Setup Moveit
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.move_group_arm = moveit_commander.MoveGroupCommander("panda_arm")
+        # self.move_group_arm.set_planner_id("RRTstarkConfigDefault")
+        self.move_group_arm.set_max_acceleration_scaling_factor(0.5)
+        self.move_group_arm.set_max_velocity_scaling_factor(0.5)
+
+        rospy.sleep(2)
+        # self.move_group_hand = moveit_commander.MoveGroupCommander("hand")
+        self.table_pose = PoseStamped()
+        self.table_pose.header.frame_id = self.robot.get_planning_frame()
+        self.table_pose.pose.position.x = 0.
+        self.table_pose.pose.position.y = 0.
+        self.table_pose.pose.position.z = 0.03
+        self.scene.add_box("table", self.table_pose, (2.0, 2.0, 0.2 ))        
         
+        # self.scene.add_box('table', 2.0, 2.0, 0.2, 0.0, 0.0, 0.03)
+
+        # pose: [0.0, 0.0, 0.03, 0, 0, 0, 1.0] # x, y, z, qx, qy, qz, qw
+        # self.move_group_arm.set_planning_time(7.0)
+        print(self.move_group_arm.get_planning_time())
         #Setup ROS
-        self.curr_state = None
+        self.curr_robot_state = self.robot.get_current_state()
+        self.curr_robot_state_ros = PoseStamped()
         self.curr_ee_goal_pos = self.goal_ee_pos_list[0]
         self.curr_ee_goal_quat = self.goal_ee_quat_list[0]
         self.curr_ee_goal = PoseStamped()
         self.curr_ee_goal.pose.position.x = self.curr_ee_goal_pos[0]
         self.curr_ee_goal.pose.position.y = self.curr_ee_goal_pos[1]
         self.curr_ee_goal.pose.position.z = self.curr_ee_goal_pos[2]
-        self.curr_ee_goal.pose.position.w = self.curr_ee_goal_pos[0]
-        self.curr_ee_goal.pose.position.x = self.curr_ee_goal_pos[1]
-        self.curr_ee_goal.pose.position.y = self.curr_ee_goal_pos[2]
-        self.curr_ee_goal.pose.position.z = self.curr_ee_goal_pos[3]
+        self.curr_ee_goal.pose.orientation.w = self.curr_ee_goal_quat[0]
+        self.curr_ee_goal.pose.orientation.x = self.curr_ee_goal_quat[1]
+        self.curr_ee_goal.pose.orientation.y = self.curr_ee_goal_quat[2]
+        self.curr_ee_goal.pose.orientation.z = self.curr_ee_goal_quat[3]
 
 
         self.rate = rospy.Rate(100)
@@ -63,43 +91,66 @@ class MoveitReacher(object):
         self.tstep = 0
         self.start_t = 0
         self.last_update_tstep = 0
+        self.plan_time = 0
         if self.log_data:
-            self.log_file = '/home/mohak/catkin_ws/src/franka_motion_control/moveit_data_{}.npz'.format(datetime.now())
+            self.log_file = '/home/mohak/catkin_ws/src/franka_motion_control/data/moveit_data_{}.npz'.format(datetime.now())
             self.tsteps = []
             self.robot_q_list = []
             self.robot_qd_list = []
             self.ee_goal_pos_list = []
             self.ee_goal_quat_list = []
-    
+            self.plan_times = []
+            self.execute_times = []
+
+
     def state_callback(self, msg):
-        self.curr_joint_state = msg
-
-    # def goal_callback(self, msg):
-    #     self.curr_ee_goal = msg
-    #     self.curr_ee_goal_pos, self.curr_ee_goal_quat = self.pose_stamped_to_np(msg)
-
+        self.curr_robot_state = msg
+        if self.tstep == 0:
+            self.start_t = rospy.get_time()
+        self.tstep = rospy.get_time() - self.start_t
+        self.robot_q_list.append(self.curr_robot_state.position)
+        self.robot_qd_list.append(self.curr_robot_state.velocity)
+        self.ee_goal_pos_list.append(self.curr_ee_goal_pos)
+        self.ee_goal_quat_list.append(self.curr_ee_goal_quat)
+        self.tsteps.append(self.tstep)
 
     def control_loop(self):
-        while not rospy.is_shutdown():
+        while True:
+
+            self.move_group_arm.set_start_state_to_current_state()
             self.move_group_arm.set_pose_target(self.curr_ee_goal.pose)
-            self.move_group_arm.go(wait=True)
-            self.executing = True
+            
+            time_before = rospy.get_time()
+            plan = self.move_group_arm.plan()
+            self.plan_times.append(rospy.get_time() - time_before)
+            
+            time_before = rospy.get_time()
+            self.move_group_arm.execute(plan, wait=True)
+            self.execute_times.append(rospy.get_time() - time_before)
+            
+            self.move_group_arm.stop()
+            self.move_group_arm.clear_pose_targets()
 
-            if self.tstep == 0:
-                self.start_t = rospy.get_time()
-            self.tstep = rospy.get_time() - self.start_t
-                                                                
-            if self.log_data:
-                self.append_data()
+            self.update_goal_from_sequence()
+            
+                                                            
+            # if self.log_data:
+            #     self.append_data()
 
-            if self.tstep - self.last_update_tstep >= 12.0:
-                self.move_group_arm.stop()
-                self.move_group_arm.clear_pose_targets()
-                self.update_goal_from_sequence()
-
-
-            self.rate.sleep() 
+            # self.rate.sleep() 
     
+    def update_goal_from_sequence(self):
+        self.curr_goal_idx = (self.curr_goal_idx + 1) % (self.num_goals)
+        self.curr_ee_goal_pos = self.goal_ee_pos_list[self.curr_goal_idx]
+        self.curr_ee_goal_quat = self.goal_ee_quat_list[self.curr_goal_idx]
+        self.curr_ee_goal.pose.position.x = self.curr_ee_goal_pos[0]
+        self.curr_ee_goal.pose.position.y = self.curr_ee_goal_pos[1]
+        self.curr_ee_goal.pose.position.z = self.curr_ee_goal_pos[2]
+        self.curr_ee_goal.pose.orientation.w = self.curr_ee_goal_quat[0]
+        self.curr_ee_goal.pose.orientation.x = self.curr_ee_goal_quat[1]
+        self.curr_ee_goal.pose.orientation.y = self.curr_ee_goal_quat[2]
+        self.curr_ee_goal.pose.orientation.z = self.curr_ee_goal_quat[3]
+
 
     def append_data(self):
         self.robot_q_list.append(self.curr_state.position)
@@ -108,26 +159,36 @@ class MoveitReacher(object):
         self.ee_goal_quat_list.append(self.curr_ee_goal_quat)
         self.tsteps.append(self.tstep)
 
+
     def close(self):
         print('Closing')
+        self.move_group_arm.stop()
+
         if self.log_data:
             print('Logging data')
             try:
                 np.savez(self.log_file, 'wb', 
                         q_robot = self.robot_q_list, 
                         qd_robot = self.robot_qd_list,
-                        qd_des_robot = self.robot_qd_des_list,
-                        q_cmd = self.command_q_list,
-                        qd_cmd = self.command_qd_list,
-                        qdd_cmd = self.command_qdd_list,
                         ee_goal_pos = self.ee_goal_pos_list,
                         ee_goal_quat = self.ee_goal_quat_list,
+                        plan_times = self.plan_times,
+                        execute_times = self.execute_times,
                         tsteps = self.tsteps)
-            except e:
+            except Exception:
                 print('Unable to save logs')
 
             print('Logs dumped')
 if __name__ == '__main__':
+    moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node("moveit_reacher", anonymous=True, disable_signals=True)
+    print('Initializing reacher')
     reacher = MoveitReacher()
-    reacher.control_loop()
+    rospy.loginfo("WARNING: This example will move the robot! \n"
+                 "Please make sure to have the user stop button at hand! \n"
+                 "Press Enter to continue... \n")
+    raw_input('')
+    try:
+        reacher.control_loop()
+    except KeyboardInterrupt:
+        reacher.close()
---
+++ src/plot_data.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+# from storm_kit.differentiable_robot_model.differentiable_robot_model import DifferentiableRobotModel
+# from storm_kit.differentiable_robot_model.coordinate_transform import matrix_to_quaternion, quaternion_to_matrix
+# from storm_kit.mpc.cost import PoseCost
+
+# import torch
+
+
+# ptcld_data = np.load('pointcloud.npz')
+# print(ptcld_data['data'])
+# input('....')
+data = np.load('../data/moveit_data_2021-05-29 16:44:09.038177_max_planning_time_5.npz')
+# err_data = np.load('mpc_error_data.npz')
+urdf_path = "/home/mohak/catkin_ws/src/franka_panda_description/robots/franka_panda_no_gripper.urdf"
+
+
+fig, ax = plt.subplots(3,1)
+fig2, ax2 = plt.subplots(4,1)
+colors = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d']
+
+for i in range(data['q_robot'].shape[1]-2):
+# i = 3
+    # if i == 0:
+    print(data['q_robot'].shape)
+    # ax[0].plot(data['q_robot_r'][:,i], color=colors[i], label='joint_{}_raw'.format(i))
+    ax[0].plot(data['tsteps'], data['q_robot'][:,i], color= colors[i])
+    # ax[0].plot(data['tsteps'], data['q_cmd'][:,i], color=colors[i], linestyle='dashed', label='joint_{}'.format(i))
+
+    # else:
+    #     ax[0].plot(data['q_robot_r'][:,i])
+    #     ax[0].plot(data['q_robot_f'][:,i])
+
+    # ax[1].plot(data['tsteps'], data['qd_robot_r'][:,i], color=colors[i])
+    ax[1].plot(data['tsteps'], data['qd_robot'][:,i], color=colors[i])
+    # ax[1].plot(data['tsteps'], data['qd_cmd'][:,i], color=colors[i], linestyle='dashed')
+
+    # ax[2].plot(data['tsteps'], data['q_robot_f'][:,i], color= colors[i])
+    # ax[2].plot(data['tsteps'], data['q_robot_r'][:,i], color= colors[i], linestyle='dashed')
+
+    # ax[3].plot(data['tsteps'], data['qd_robot_f'][:,i], color= colors[i])
+    # ax[3].plot(data['tsteps'], data['qd_robot_r'][:,i], color= colors[i], linestyle='dashed')
+
+    # ax[2].plot(data['tsteps'], data['qdd_cmd'][:,i], color=colors[i], linestyle='dashed')
+
+ax[0].set_title('Robot Joint Positions')
+ax[1].set_title('Robot Joint Velocities')
+# ax[2].set_title('Filtered v/s raw (positions)')
+# ax[3].set_title('Filtered v/s raw (velocities)')
+# ax[3].set_title('MPC Commanded Joint Positions')
+ax[2].set_title('MPC Commanded Joint Accs')
+
+#Load robot model
+# tensor_args = {'device':"cpu", 'dtype':torch.float32}
+
+# robot_model = DifferentiableRobotModel(urdf_path, None, 
+#                     tensor_args=tensor_args)
+
+
+# vec_weight = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+# weight = [1.0,1.0]
+# position_gaussian_params = {'n':0, 'c':0.2, 's':0.0, 'r':10.0}
+# orientation_gaussian_params =  {'n':0, 'c':0.2, 's':0.0, 'r':10.0}
+# hinge_val =  -1
+# convergence_val =  [0.0, 0.0] # orientation, position
+
+# tensor_args = {'device':"cpu", 'dtype':torch.float32}
+# pose_cost_fn =  PoseCost(weight, vec_weight, position_gaussian_params, 
+#                          orientation_gaussian_params, tensor_args, 
+#                          hinge_val, convergence_val)
+
+
+#Run FK to get ee pos, quat
+# q = torch.tensor(data['q_robot'])
+# qd = torch.tensor(data['qd_robot'])
+# ee_pos, ee_rot = robot_model.compute_forward_kinematics(q, qd, link_name="ee_link")
+# ee_quat = matrix_to_quaternion(ee_rot)
+# print(ee_pos.shape, ee_quat.shape)
+
+# ee_goal_pos_torch = torch.tensor(data['ee_goal_pos'])
+# # ee_goal_rot_torch = quaternion_to_matrix(torch.tensor(data['ee_goal_quat']))
+# ee_goal_quat_torch = torch.tensor(data['ee_goal_quat'])
+# ee_dist_error = torch.norm(ee_goal_pos_torch - ee_pos, dim=-1)
+
+# term1 = torch.norm(ee_goal_quat_torch - ee_quat, dim=-1)
+# term2 = torch.norm(ee_goal_quat_torch + ee_quat, dim=-1)
+# ee_quat_error = (100.0/np.sqrt(2.0)) * torch.min(term1, term2)
+
+
+
+
+# cost, ee_rot_error, ee_dist_error = pose_cost_fn.forward(ee_pos, ee_rot, ee_goal_pos_torch, ee_goal_rot_torch)
+# preint(ee_rot_error.shape, ee_dist_error.shape)
+
+# cart_labels = {'x': '#1b9e77', 'y': '#d95f02', 'z': '#7570b3'}
+# quat_labels = {'x': '#1b9e77', 'y': '#d95f02', 'z': '#7570b3', 'w': '#e7298a'}
+# for i in range(3):
+#     key = list(cart_labels.keys())[i]
+#     ax2[0].plot(data['tsteps'], ee_pos[:,i], label=key, color=cart_labels[key])
+#     ax2[0].plot(data['tsteps'], data['ee_goal_pos'][:,i], linestyle='dashed', label=key+"_des", color=cart_labels[key])
+
+# for i in range(4):
+#     key = list(quat_labels.keys())[i]
+#     ax2[1].plot(data['tsteps'], ee_quat[:,i], label=key, color=quat_labels[key])
+#     ax2[1].plot(data['tsteps'], data['ee_goal_quat'][:,i], linestyle='dashed', label=key+"_des", color=quat_labels[key])
+
+# ax2[2].plot(data['tsteps'], ee_dist_error)
+# ax2[3].plot(data['tsteps'], ee_quat_error)
+
+# ax2[0].set_title('EE cartesian')
+# ax2[1].set_title('EE ee_quaternion')
+# ax2[2].set_title('EE L2 error')
+# ax2[3].set_title('EE rot error')
+# ax[0].legend()
+# ax2[0].legend()
+# ax2[1].legend()
+# plt.tight_layout()
+plt.show()
+data.close()
